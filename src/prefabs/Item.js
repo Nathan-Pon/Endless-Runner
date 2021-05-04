e0a08810bcbb16bc4ece0d4d7a495fc86b10d506
Nathan Pon <nathanpon98@gmail.com>
2021-05-04 14:39:11 -0700

    Custom Code

--- src/prefabs/Item.js
+++ src/prefabs/Item.js
@@ -8,5 +8,26 @@
 */
 
 class Item extends Phaser.Physics.Arcade.Sprite {
+    constructor(scene, x, y, texture, velocity =500) {
+        super( scene, game.config.width , Phaser.Math.Between(128/2, game.config.height - 128/2), 'items');
+
+         // add object to the existing scene
+         scene.add.existing(this);
+         scene.physics.add.existing(this);
+         this.setOffset(32, -8);
+         this.setVelocityX(-velocity);
+         this.newItem = true;
     
+         if(this.x < -this.width) {
+            this.destroy();
+         }        
+    }     
+    
+    update() {
+        if(this.newItem && this.x < game.config.width/2) {
+            this.newItem = false;
+            this.scene.addItem(this.velocity);
+          
+        }
+    }
 }
